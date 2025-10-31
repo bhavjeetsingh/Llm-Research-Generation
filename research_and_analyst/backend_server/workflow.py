e28ede2aecdf8b5a750ee3a398f202ebc0d18e64
bhavjeet <bhavjeetsingh784@gmail.com>
2025-11-01 00:50:06 +0530

    workflow  updated part 1

--- research_and_analyst/backend_server/workflow.py
+++ research_and_analyst/backend_server/workflow.py
@@ -4,25 +4,44 @@ from datetime import datetime
 from typing import Optional
 from langgraph.types import Send
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, "../../"))
+sys.path.append(project_root)
+
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, get_buffer_string
-from langchain_community.tools.tavily_search import TavilySEarchResults
+from langchain_community.tools.tavily_search import TavilySearchResults
 
-from docx import Document
+from docx import document
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
 from research_and_analyst.backend_server.models import(
     Analyst,
-    Perspective,
-    GenerateAnalystState,
+    Perspectives,
+    GenerateAnalystsState,
     InterviewState,
-    ResearchGrpahState,
+    ResearchGraphState,
 )
 from research_and_analyst.utils.model_loader import ModelLoader
 
-
+def buil_interview_graph(llm,tavily_search=None):
+    """
+    Args:
+        llm (_type_): _description_
+        tavily_search (_type_, optional): _description_. Defaults to None.
+    """
+    def generation_question(state:InterviewState):
+        pass
+    def search_web(state:InterviewState):
+        pass
+    def generate_answer(state:InterviewState):
+        pass
+    def save_interview(state:InterviewState):
+        pass
+    def write_section(state:InterviewState):
+        pass
 class AutonomousReportGenerator:
     def __init__(self):
         """_summary_
@@ -77,4 +96,7 @@ class AutonomousReportGenerator:
 if __name__ == "__main__":
         """_summary_
         """
-        pass
+        llm = ModelLoader().load_llm()
+        print(llm.invoke('hello').content)
+        reporter = AutonomousReportGenerator()
+        reporter.build_graph()
--- research_and_analyst/utils/model_loader.py
+++ research_and_analyst/utils/model_loader.py
@@ -2,17 +2,19 @@ import os
 import sys
 import json
 from dotenv import load_dotenv
-from utils.config_loader import load_config
+from research_and_analyst.utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
-from logger import GLOBAL_LOGGER as log
-from exception.custom_exception import ResearchAnalystException
+from research_and_analyst.logger import GLOBAL_LOGGER as log
+from research_and_analyst.exception.custom_exception import ResearchAnalystException
 import asyncio
+from dotenv import load_dotenv
 
 
 class ApiKeyManager:
     def __init__(self):
+        load_dotenv()  # Load environment variables from .env file
         self.api_keys = {
             "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
             "GOOGLE_API_KEY": os.getenv("GOOGLE_API_KEY"),
@@ -72,7 +74,7 @@ class ModelLoader:
         Load and return the configured LLM model.
         """
         llm_block = self.config["llm"]
-        provider_key = os.getenv("LLM_PROVIDER", "groq")
+        provider_key = os.getenv("LLM_PROVIDER", "google")
 
         if provider_key not in llm_block:
             log.error("LLM provider not found in config", provider=provider_key)
